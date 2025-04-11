cd752284725b422d40c7df7e8ab246587c93563f
Partha Maity <parthamaity2004@gmail.com>
2025-04-12 00:13:28 +0530

    Show to increment value in Ui using hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,35 @@
-import { useState } from "react";
+import { useState } from "react" // this is Hook library
 
 
 
 function App() {
 
-  let counter=5;
+    let [val, setCounter] = useState(5); // This 5 is default value
+    // val and setCounter is a variable name
+
+  // let counter=5;
 
   const addValue = () =>{
-    counter+=1;
-    console.log("value added", counter);
+    // counter+=1;
+    if(val<20)
+    {
+      val=val+1;
+      setCounter(val);
+    }
+    console.log("value added", val);
   }
-
+  function RemoveVal(){
+    val=val-1;
+    setCounter(val);
+  }
   return (
     <>
       <h1>Chai Aur React</h1>
-      <h2>Counter: {counter}</h2>
+      <h2>Counter: {val}</h2>
 
-      <button onClick={addValue}>Add value</button>
+      <button onClick={addValue}>Add value {val}</button>
       <br />
-      <button>Remove value</button>
+      <button onClick={RemoveVal}>Remove value {val}</button>
     </>
   )
 }
